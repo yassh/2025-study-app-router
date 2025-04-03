bc0242c563dc120f861e1cdf194c0159a7c37f14
yassh <7814130+yassh@users.noreply.github.com>
2025-04-03 16:40:26 +0900

    fix random-todo/page.tsx

--- src/app/random-todo/page.tsx
+++ src/app/random-todo/page.tsx
@@ -4,12 +4,12 @@ import { getRandomTodo } from "./_resources/todos";
 // > データフェッチをサポートしながらもレンダリングの範囲ではコンポーネントの純粋性を保てるよう設計されています。
 // ☞ https://zenn.dev/akfm/books/nextjs-basic-principle/viewer/part_4_pure_server_components#%E3%83%87%E3%83%BC%E3%82%BF%E3%83%95%E3%82%A7%E3%83%83%E3%83%81%E3%81%AE%E4%B8%80%E8%B2%AB%E6%80%A7
 
-export async function ComponentA() {
+async function ComponentA() {
   const todo = await getRandomTodo();
   return <div>{JSON.stringify(todo)}</div>;
 }
 
-export async function ComponentB() {
+async function ComponentB() {
   const todo = await getRandomTodo();
   return <div>{JSON.stringify(todo)}</div>;
 }
